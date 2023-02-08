e21785428a78f7797dcdcbc3413709524398c049
LuisDutra <luisvinicius99@gmail.com>
2023-02-08 17:53:00 -0300

    add ApiKey attribute

--- Configuration.cs
+++ Configuration.cs
@@ -2,5 +2,9 @@
 
 public static class Configuration
 {
-    public static string JwtKey { get; set; } = "`0G3bQ2y,hC%0SW*$va`23c;!s$3FE1j";
+    public static string JwtKey = "`0G3bQ2y,hC%0SW*$va`23c;!s$3FE1j";
+
+    public static string ApiKeyName = "api_key";
+    
+    public static string ApiKey = "Quiporro_temporibus-in-laborumT%architecto==sit";
 }
