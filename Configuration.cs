00ce65fbfff30141dc56148260c1dbeaa454464d
LuisDutra <luisvinicius99@gmail.com>
2023-02-13 09:44:05 -0300

    Organize Program and add system variable to appsettings

--- Configuration.cs
+++ Configuration.cs
@@ -7,4 +7,17 @@ public static class Configuration
     public static string ApiKeyName = "api_key";
     
     public static string ApiKey = "Quiporro_temporibus-in-laborumT%architecto==sit";
+
+    public static SmtpConfiguration Smtp = new();
+    
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+
+        public int Port { get; set; } = 25;
+        
+        public string UserName { get; set; }
+        
+        public string Password { get; set; }
+    }
 }
